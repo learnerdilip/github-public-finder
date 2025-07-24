8e71b513fea7155d66bc1a29664be9e747770dc9
Dilip <diliptalks@gmail.com>
2025-07-24 18:10:05 +0200

    initial files, cleared boilerplate and added tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className="bg-red">Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
+
+export default App;
